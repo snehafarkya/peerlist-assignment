493f741cd75401d353dd467cdddb565632ee287b
snehafarkya <snhafarkya@gmail.com>
2024-03-15 23:35:16 +0530

    functionality done

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -12,7 +12,9 @@ import { useState } from "react";
 import Image from "next/image";
 
 import user from "../../../public/images/card-user.svg";
+import minus from "../../../public/images/minus.svg";
 import tick from "../../../public/images/tick.svg";
+import logo from "../../../public/images/circlelogo.svg";
 import verified from "../../../public/images/verified.svg";
 
 const Card = ({
@@ -28,7 +30,9 @@ const Card = ({
   number,
   shortlist,
   handleDragStart,
+  applied,
   handleDragEnd,
+  icon
 }) => {
   const [isDragging, setIsDragging] = useState(false);
 
@@ -43,13 +47,14 @@ const Card = ({
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
       onDrag={handleDrag}
-      className={`p-6 m-2 border-[#E1E4E8] border  bg-white rounded-md flex flex-col justify-start gap-1  text-black transition-all duration-300 ${
+      className={`py-6 px-4 m-2 mx-1 border-[#E1E4E8] border  bg-white rounded-md flex flex-col justify-start gap-2  text-black transition-all duration-300 ${
         isDragging ? "opacity-50" : ""
       }`}
     >
       <div className="flex justify-between">
-      <Image src={user} alt="user" />
-      <p className="text-[10px] text-[#6A737D]">Applied 1d ago</p>
+      {icon}
+      {applied ? <p className="text-[10px] text-[#6A737D]">{applied}</p> : <p className="bg-[#E2F5EA] border rounded-3xl text-[10px] h-min p-1 flex justify-center items-center gap-1 text-[#219653] font-semibold"> <Image src={logo} alt="" className="h-4 w-4"/> Matched</p>}
+
       </div>
       <div className="flex flex-col">
         <p className="text-sm font-semibold flex gap-1 items-center">
@@ -62,25 +67,25 @@ const Card = ({
       </div>
       <div className="flex justify-between flex-wrap gap-y-2">
         {exp ? (
-          <div className="flex gap-1 flex-col">
+          <div className="flex  flex-col">
             <p className="text-[10px] ">Experience</p>
             <p className="text-xs font-semibold">{exp}</p>
           </div>
         ) : null}
         {offer ? (
-          <div className="flex gap-1 flex-col">
+          <div className="flex flex-col">
             <p className="text-[10px] ">Holding offer?</p>
             <p className="text-xs font-semibold">{offer}</p>
           </div>
         ) : null}
         {notice ? (
-          <div className="flex gap-1 flex-col">
+          <div className="flex flex-col">
             <p className="text-[10px] ">Notice Period</p>
             <p className="text-xs font-semibold">{notice}</p>
           </div>
         ) : null}
         {email && number ? (
-          <div className="flex gap-1 flex-col">
+          <div className="flex  flex-col">
             <p className="text-[10px] ">Contact</p>
             <p className="text-xs font-semibold">{email}</p>
             <p className="text-xs font-semibold">{number}</p>
@@ -99,7 +104,7 @@ const Card = ({
         {category === 'rejected' ?(
           <div className="flex gap-1 items-center text-[10px]">
           {shortlist && <>
-            <Image src={tick} alt="" /> Rejected by <b>{shortlist}</b> on 15
+            <Image src={minus} alt="" /> Rejected by <b>{shortlist}</b> on 15
             Dec 2023</>
           }
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,146 +1,173 @@
 import Image from "next/image";
-import logo from '../../public/images/logo.png'
-import circlelogo from '../../public/images/circlelogo.svg'
-import arrow from '../../public/images/downarrow.svg'
-import akash from '../../public/images/akash.svg'
-import scroll from '../../public/images/scroll.svg'
-import project from '../../public/images/project.svg'
-import inbox from '../../public/images/inbox.svg'
-import job from '../../public/images/job.svg'
-import search from '../../public/images/search.svg'
-import network from '../../public/images/network.svg'
-import user from '../../public/images/user.svg'
-import loom from '../../public/images/Loom - jpeg 0.svg'
-import share from '../../public/images/share.svg'
-import sharee from '../../public/images/sharee.svg'
-import threedot from '../../public/images/threedots.svg'
-import edit from '../../public/images/edit.svg'
+import logo from "../../public/images/logo.png";
+import circlelogo from "../../public/images/circlelogo.svg";
+import arrow from "../../public/images/downarrow.svg";
+import akash from "../../public/images/akash.svg";
+import scroll from "../../public/images/scroll.svg";
+import project from "../../public/images/project.svg";
+import inbox from "../../public/images/inbox.svg";
+import job from "../../public/images/job.svg";
+import search from "../../public/images/search.svg";
+import network from "../../public/images/network.svg";
+import user from "../../public/images/user.svg";
+import loom from "../../public/images/Loom - jpeg 0.svg";
+import share from "../../public/images/share.svg";
+import sharee from "../../public/images/sharee.svg";
+import threedot from "../../public/images/threedots.svg";
+import edit from "../../public/images/edit.svg";
 import Card from "./components/card";
-import CardContainer from './components/cardContainer';
+import CardContainer from "./components/cardContainer";
 export default function Home() {
   const asideMenu = [
     {
       icon: scroll,
-      label:'Scroll',
-      social:edit,
-      stat:'78',
-      value:'Candidates',
+      label: "Scroll",
+      social: edit,
+      stat: "78",
+      value: "Candidates",
     },
     {
       icon: project,
-      label:'Project',
-      social:share,
-      stat:'68',
-      value:'Applied w/ Peerlist',
+      label: "Project",
+      social: share,
+      stat: "68",
+      value: "Applied w/ Peerlist",
     },
     {
       icon: inbox,
-      social:sharee,
-      label:'Inbox',
-      stat:'2872',
-      value:'Views',
+      social: sharee,
+      label: "Inbox",
+      stat: "2872",
+      value: "Views",
     },
     {
       icon: job,
-      social:threedot,
-      label:'Job',
+      social: threedot,
+      label: "Job",
     },
     {
       icon: search,
-      label:'Search',
+      label: "Search",
     },
     {
       icon: network,
-      label:'Network',
+      label: "Network",
     },
-  ]
+  ];
   return (
-   <>
-   <main className="md:px-40 flex  justify-start border-r border-[#E1E4E8]  items-start bg-white h-full min-h-screen">
-    <aside className="flex  flex-col py-4 sticky top-2 justify-between w-[212px] border-r  pr-6 h-auto min-h-screen">
-      <div className="">
-    <Image src={logo} alt="logo" height={32} />
-    <div className="flex flex-col gap-2 my-6">
-      {asideMenu.map((item,index)=>{
-        return(
-          <div key={index} className="flex items-center gap-1 h-10">
-            <Image src={item.icon} alt="Icon here"/>
-            <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">{item.label}</h4>
+    <>
+      <main className="md:px-40 flex  justify-start border-r border-[#E1E4E8]  items-start bg-white h-full min-h-screen">
+        <aside className="flex  flex-col py-4 sticky top-2 justify-between w-[212px] border-r  pr-6 h-auto min-h-screen">
+          <div className="">
+            <Image src={logo} alt="logo" height={32} />
+            <div className="flex flex-col gap-2 my-6">
+              {asideMenu.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="flex items-center gap-1 h-10 cursor-pointer"
+                  >
+                    <Image src={item.icon} alt="Icon here" />
+                    <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">
+                      {item.label}
+                    </h4>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-1 cursor-pointer">
+                <Image src={user} alt="Icon here" />
+                <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">
+                  Yogini
+                </h4>
+              </div>
+              <div className="flex  items-start gap-1 cursor-pointer">
+                <Image src={loom} alt="Icon here" />
+                <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">
+                  Loom
+                  <p className="text-[10px] ">Manage jobs, teams, & more →</p>
+                </h4>
+              </div>
+            </div>
           </div>
-        )
-      })}
-    </div>
-    <div className="flex flex-col gap-4">
-    <div  className="flex items-center gap-1">
-            <Image src={user} alt="Icon here"/>
-            <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">Yogini</h4>
-          </div>
-          <div  className="flex  items-start gap-1">
-            <Image src={loom} alt="Icon here"/>
-            <h4 className="font-semibold text-sm text-[#0D0D0D] leading-5">Loom
-            <p className="text-[10px] ">Manage jobs, teams, & more →</p>
-            </h4>
-          </div>
-    </div>
-    </div>
-    <div className="flex flex-col gap-2">
-      <p className="text-[10px] font-semibold  text-[#444D56] w-[188px]">Blog • Help Center • Feedback • Code of Conduct • Privacy • T&C</p>
-      <p className="text-[#444D56] text-[10px]">© 2023 Peerlist Inc.</p>
-    </div>
-    </aside>
-    <div className=" flex flex-col gap-6  w-full ">
-      <div className="flex p-6 flex-col gap-6 bg-[#FAFBFC] w-full border-b border-r border-[#E1E4E8]">
-      <div className="flex justify-between w-full items-start">
-        <div className="flex gap-4 items-center">
-          <Image src={circlelogo} alt="circlelogo" className="border p-1 border-[#E1E4E8] rounded-2xl" />
-          <div className="flex flex-col">
-            <p className="text-lg flex items-center gap-1 font-semibold text-[#0D0D0D] ">Software Engineer, Front End 
-            <span>
-              <Image src={arrow} alt="arrow"/>
-            </span>
+          <div className="flex flex-col gap-2">
+            <p className="text-[10px] font-semibold  text-[#444D56] w-[188px]">
+              Blog • Help Center • Feedback • Code of Conduct • Privacy • T&C
             </p>
-            <p className=" text-sm text-[#0D0D0D]">at Dyte • Full time • Remote (United States, Canada)</p>
+            <p className="text-[#444D56] text-[10px]">© 2023 Peerlist Inc.</p>
           </div>
-        </div>
-        <div className="gap-2 flex">
-        {asideMenu.map((item,index)=>{
-          return(
-            <>
-            {item.social? (
-              <div className="rounded-full  p-1 border">
-                <Image src={item.social} alt="" />
+        </aside>
+        <div className=" flex flex-col gap-6 w-full border-r border-[#E1E4E8] border-solid ">
+          <div className="flex p-6 flex-col gap-6 bg-[#FAFBFC] w-full border-b border-r border-[#E1E4E8]">
+            <div className="flex justify-between w-full items-start">
+              <div className="flex gap-4 items-center">
+                <Image
+                  src={circlelogo}
+                  alt="circlelogo"
+                  className="border p-1 border-[#E1E4E8] rounded-2xl"
+                />
+                <div className="flex flex-col">
+                  <p className="text-lg flex items-center gap-1 font-semibold text-[#0D0D0D] ">
+                    Software Engineer, Front End
+                    <span>
+                      <Image src={arrow} alt="arrow" />
+                    </span>
+                  </p>
+                  <p className=" text-sm text-[#0D0D0D]">
+                    at Dyte • Full time • Remote (United States, Canada)
+                  </p>
+                </div>
               </div>
-            ):null}
-            </>
-          )
-        })}
-        </div>
-        
-      </div>
-      <div className="flex justify-between w-full pl-16">
-        <div className="flex gap-4 ">
-        {asideMenu.map((item,index)=>{
-          return(
-            <>
-            {item.stat? (
-              <div className="flex gap-1 justify-center">
-                <p className="text-xs font-semibold text-[#0D0D0D]">{item.stat}</p>
-                <p className="text-xs font-normal text-[#0D0D0D]">{item.value}</p>
+              <div className="gap-2 flex">
+                {asideMenu.map((item, index) => {
+                  return (
+                    <>
+                      {item.social ? (
+                        <div
+                          className="rounded-full cursor-pointer  p-1 border"
+                          style={{ cursor: "pointer" }}
+                        >
+                          <Image
+                            src={item.social}
+                            alt=""
+                            className="cursor-pointer "
+                          />
+                        </div>
+                      ) : null}
+                    </>
+                  );
+                })}
               </div>
-            ):null}
-            </>
-          )
-        })}
+            </div>
+            <div className="flex justify-between w-full pl-16">
+              <div className="flex gap-4 ">
+                {asideMenu.map((item, index) => {
+                  return (
+                    <>
+                      {item.stat ? (
+                        <div className="flex gap-1 justify-center">
+                          <p className="text-xs font-semibold text-[#0D0D0D]">
+                            {item.stat}
+                          </p>
+                          <p className="text-xs font-normal text-[#0D0D0D]">
+                            {item.value}
+                          </p>
+                        </div>
+                      ) : null}
+                    </>
+                  );
+                })}
+              </div>
+              <p className="text-[10px] text-[#0D0D0D] flex justify-center gap-1 items-center">
+                Posted <b>1d ago</b> by <Image src={akash} alt="" />{" "}
+                <b>Akash Bhadange</b>
+              </p>
+            </div>
+          </div>
+          <CardContainer />
         </div>
-        <p className="text-[10px] text-[#0D0D0D] flex justify-center gap-1 items-center">Posted <b>1d ago</b> by <Image src={akash} alt=""/> <b>Akash Bhadange</b></p>
-      </div>
-      </div>
-      <CardContainer />
-
-    </div>
-
-   </main>
-
-   </>
+      </main>
+    </>
   );
 }
